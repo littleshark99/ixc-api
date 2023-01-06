a935aa6b94617d1bf1eb2070cf84bf09f22f606e
Rafael Clare <78705877+littleshark99@users.noreply.github.com>
2023-01-06 16:07:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-import {request} from "https://unpkg.com/request@2.88.2/index.js";
-
 
 
 const clientename = document.querySelector('#name');
-const cleinteativo = document.querySelector('#ativo');
+const clienteativo = document.querySelector('#ativo');
 const clienteonline = document.querySelector('#online');
 const statusconexao = document.querySelector('#ultimaconexao');
-const interfaces = document.querySelector('#interface');
-const endereco = document.querySelector('#endereco');
-const bairro = document.querySelector('#bairro');
-const cidade = document.querySelector('#cidade');
+const interface = document.querySelector('#interface');
+const lugar = document.querySelector('#endereco');
+const lougradouro = document.querySelector('#bairro');
+const estado = document.querySelector('#cidade');
 const tablebody = document.querySelector('#tabela');
 
 
 
 
-let token = 'TOKEN';
+
+let token = '116:49163d784512d799beb6eb54ef0d3b2605b5c5fd1c9e64800ff8955d55ddce1e';
 let options = { method: 'GET',
-  url: 'https://HOST/webservice/v1/radusuarios',
+  url: 'https://ixc.vntinternet.com.br/webservice/v1/radusuarios',
   headers:
    {
      'Content-Type': 'application/json',
@@ -57,8 +56,36 @@ clientes.map((cliente) =>{
     const bairro = document.createElement("td");
     const cidade = document.createElement("td");
     
-    nome.textContent = cliente.conexao
+    nome.textContent = cliente.clientename
     linha.appendChild(nome);
     tablebody.appendChild(linha);
-    
+
+    ativo.textContent = cliente.clienteativo
+    linha.appendChild(ativo);
+    tablebody.appendChild(linha);
+
+    online.textContent = cliente.clienteonline
+    linha.appendChild(online);
+    tablebody.appendChild(linha);
+
+    status.textContent = cliente.statusconexao
+    linha.appendChild(status);
+    tablebody.appendChild(linha);
+
+    interfaces.textContent = cliente.interface
+    linha.appendChild(interfaces);
+    tablebody.appendChild(linha);
+
+    endereco.textContent = cliente.lugar
+    linha.appendChild(endereco);
+    tablebody.appendChild(linha);
+
+    bairro.textContent = cliente.lougradouro
+    linha.appendChild(bairro);
+    tablebody.appendChild(linha);
+
+    cidade.textContent = cliente.estado
+    linha.appendChild(cidade);
+    tablebody.appendChild(linha);
+
 });
