65aa65b4b4731786c094c846410bcf175d12a472
Rafael Clare <78705877+littleshark99@users.noreply.github.com>
2023-01-11 17:42:13 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-
-const hostname = '127.0.0.1'
-const port = 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('servidor do pai ta on \n')
-  
-}) 
-
-app.get('/user', function (req, res) {
-    res.send('Lista de usuários');
-})
+const express = require('express');
+const app = express();
 
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+ "/src/index.html");
+
+});
+
+
+
+app.listen(3000, () => {
+    console.log("servidor inciado na porta 3000 : http://localhost:3000/");
+});
---
+++ view/script.js
@@ -0,0 +1,88 @@
+const clientename = document.querySelector('#name');
+const clienteativo = document.querySelector('#ativo');
+const clienteonline = document.querySelector('#online');
+const statusconexao = document.querySelector('#ultimaconexao');
+const interface = document.querySelector('#interface');
+const lugar = document.querySelector('#endereco');
+const lougradouro = document.querySelector('#bairro');
+const estado = document.querySelector('#cidade');
+const tablebody = document.querySelector('#tabela');
+
+
+
+
+let token = 'TOKEN';
+let options = { method: 'GET',
+  url: 'https://HOST/webservice/v1/radusuarios',
+  headers:
+   {
+     'Content-Type': 'application/json',
+     Authorization: 'Basic ' + new Buffer.from(token).toString('base64'),
+     ixcsoft: 'listar'
+   },
+  body:
+   { qtype: 'radusuarios.id',
+     query: '0',
+     oper: '>',
+     page: '1',
+     rp: '20',
+     sortname: 'radusuarios.id',
+     sortorder: 'desc'
+   },
+  json: true
+  };
+//CLIENTE
+  
+
+const clientes = request(options, function (error, response, body)  {
+  if (error) throw new Error(error);
+
+  console.log("erro na integração");
+});
+
+
+clientes.map((cliente) =>{
+
+    const linha = document.createElement("tr");
+    const nome = document.createElement("td");
+    const ativo = document.createElement("td");
+    const online = document.createElement("td");
+    const status = document.createElement("td");
+    const interfaces = document.createElement("td");
+    const endereco = document.createElement("td");
+    const bairro = document.createElement("td");
+    const cidade = document.createElement("td");
+    
+    nome.textContent = cliente.clientename
+    linha.appendChild(nome);
+    tablebody.appendChild(linha);
+
+    ativo.textContent = cliente.clienteativo
+    linha.appendChild(ativo);
+    tablebody.appendChild(linha);
+
+    online.textContent = cliente.clienteonline
+    linha.appendChild(online);
+    tablebody.appendChild(linha);
+
+    status.textContent = cliente.statusconexao
+    linha.appendChild(status);
+    tablebody.appendChild(linha);
+
+    interfaces.textContent = cliente.interface
+    linha.appendChild(interfaces);
+    tablebody.appendChild(linha);
+
+    endereco.textContent = cliente.lugar
+    linha.appendChild(endereco);
+    tablebody.appendChild(linha);
+
+    bairro.textContent = cliente.lougradouro
+    linha.appendChild(bairro);
+    tablebody.appendChild(linha);
+
+    cidade.textContent = cliente.estado
+    linha.appendChild(cidade);
+    tablebody.appendChild(linha);
+
+});
