12f8cdcce6171322280e9b4e4ec6acb475698160
Rafael Clare <78705877+littleshark99@users.noreply.github.com>
2023-01-16 16:01:09 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const { dirname } = require('path');
 const app = express();
+const path = require("path");
 
 
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname+ "/src/index.html");
-
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 
